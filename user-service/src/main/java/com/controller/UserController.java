eeb9aeab0e3d261c9a48a6ed62e2a590a0bd2db0
Rohith Kumar N <rk.rohithkumar97@gmail.com>
2022-10-12 13:45:50 +0530

    retry mechanism implemented

--- user-service/src/main/java/com/controller/UserController.java
+++ user-service/src/main/java/com/controller/UserController.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import com.model.OrderDTO;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -24,11 +26,14 @@ public class UserController {
 
     private static final String BASEURL = "http://localhost:9191/orders";
     private static final String USER_SERVICE = "userService";
+    private int attempt = 1;
 
     @GetMapping("/displayOrders")
-    @CircuitBreaker(name = USER_SERVICE, fallbackMethod = "getAllAvailableProducts")
+//    @CircuitBreaker(name = USER_SERVICE, fallbackMethod = "getAllAvailableProducts")
+    @Retry(name = USER_SERVICE, fallbackMethod = "getAllAvailableProducts")
     public List<OrderDTO> displayOrders(@RequestParam("category") String category) {
         String url = category == null ? BASEURL : BASEURL + "/" + category;
+        System.out.println("retry method called " + attempt++ + " times " + " at " + new Date());
         return restTemplate.getForObject(url, ArrayList.class);
     }
 
